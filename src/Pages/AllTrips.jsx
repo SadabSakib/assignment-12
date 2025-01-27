b40fc99af0a050147194bf47f8beb13022b71a18
SadabSakib <sadabsakib9@gmail.com>
2025-01-27 22:47:58 +0600

    something something

--- src/Pages/AllTrips.jsx
+++ src/Pages/AllTrips.jsx
@@ -3,7 +3,7 @@ import useAxiosPublic from '../components/hooks/useAxiosPublic';
 
 const AllTrips = () => {
     const axiosPublic=useAxiosPublic()
-    const [packages,setPackages]=useState([])
+    const [singlePackage, setPackages] = useState([]);
         useEffect(() => {
           axiosPublic.get("/api/allPackages/random").then(res => {
             setPackages(res.data)
@@ -12,9 +12,26 @@ const AllTrips = () => {
         },[axiosPublic])
      
     return (
-        <div>
-            AllTrips
+      <div className="card">
+        <img
+          src={singlePackage.photoURL}
+          alt={singlePackage.title}
+          className="card-img-top"
+        />
+        <div className="card-body">
+          <h5 className="card-title">{singlePackage.title}</h5>
+          <p className="card-text">Tour Type: {singlePackage.tourType}</p>
+          <p className="card-text">Price: ${singlePackage.price}</p>
+          <button
+            onClick={() =>
+              (window.location.href = `/package/${singlePackage._id}`)
+            }
+            className="btn btn-primary"
+          >
+            View Details
+          </button>
         </div>
+      </div>
     );
 };
 
